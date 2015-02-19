fe5745935437495d310b27ecbc7d66d3f34d53a6
george <g.rabinov85@gmail.com>
2015-02-19 13:56:10 +0200

    Project Created
    First sync

---
+++ CHI-SocialNetwork/CHI-SocialNetwork/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CHI_SocialNetwork.Startup))]
+namespace CHI_SocialNetwork
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
