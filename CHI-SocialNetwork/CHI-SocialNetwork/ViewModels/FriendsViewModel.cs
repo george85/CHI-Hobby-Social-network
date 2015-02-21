37a8130efd48068e01e4f2db12387a333e4970e5
george <g.rabinov85@gmail.com>
2015-02-21 21:06:47 +0200

    ViewModels

---
+++ CHI-SocialNetwork/CHI-SocialNetwork/ViewModels/FriendsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CHI_SocialNetwork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CHI_SocialNetwork.ViewModels
+{
+    public class FriendsViewModel
+    {
+        public User User { get; set; }
+        public IEnumerable<User> Friends { get; set; }
+    }
+}
---
+++ CHI-SocialNetwork/CHI-SocialNetwork/ViewModels/LoginSignupViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CHI_SocialNetwork.ViewModels
+{
+    public class LoginSignupViewModel
+    {
+        public LoginViewModel Login { get; set; }
+        public SignupViewModel Signup { get; set; }
+    }
+}
